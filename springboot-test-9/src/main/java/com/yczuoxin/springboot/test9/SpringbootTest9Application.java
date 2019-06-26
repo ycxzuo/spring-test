7836f2fa5c7f66fa2ffc79058bd350d2a9bed731
yczuoxin <343720929@qq.com>
2019-06-26 23:30:53 +0800

    elasticsearch学习笔记

---
+++ springboot-test-9/src/main/java/com/yczuoxin/springboot/test9/SpringbootTest9Application.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.springboot.test9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest9Application.class, args);
+    }
+
+}
