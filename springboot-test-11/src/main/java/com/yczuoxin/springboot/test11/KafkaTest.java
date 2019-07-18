f551d641b92c2a5ad2759d6051633949a11ce5ac
ycxzuo <343720929@qq.com>
2019-07-18 13:27:55 +0800

    kafka消息

---
+++ springboot-test-11/src/main/java/com/yczuoxin/springboot/test11/KafkaTest.java
@@ -0,0 +1,24 @@
+package com.yczuoxin.springboot.test11;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class KafkaTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","47.106.80.100:9092,47.106.146.28:9092,47.106.127.193:9092");
+        properties.put("key.serializer", StringSerializer.class);
+        properties.put("value.serializer",StringSerializer.class);
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        ProducerRecord<String, String> record = new ProducerRecord<>("test-1",0,"message", "test");
+        Future<RecordMetadata> send = producer.send(record);
+        RecordMetadata recordMetadata = send.get();
+        System.out.println(recordMetadata);
+    }
+}
