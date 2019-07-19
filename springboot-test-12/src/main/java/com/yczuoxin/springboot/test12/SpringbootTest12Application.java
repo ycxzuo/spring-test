cf5feba6c4a3a1473e88bd53d24e14401a10f4de
zuoxin <343720929@qq.com>
2019-07-19 18:21:00 +0800

    搭建

---
+++ springboot-test-12/src/main/java/com/yczuoxin/springboot/test12/SpringbootTest12Application.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.springboot.test12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest12Application.class, args);
+    }
+
+}
