d74ee7e3632b08fbfe20e4b9f91b9320d566d51a
zuoxin <343720929@qq.com>
2019-07-25 21:42:01 +0800

    升级pom

--- springboot-test-12/src/main/java/com/yczuoxin/springboot/test12/controller/ValidationController.java
+++ springboot-test-12/src/main/java/com/yczuoxin/springboot/test12/controller/ValidationController.java
@@ -1,19 +1,17 @@
 package com.yczuoxin.springboot.test12.controller;
 
 import com.yczuoxin.springboot.test12.domain.Person;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 public class ValidationController {
 
     @PostMapping("/person")
-    public Person save(@Valid @RequestBody Person person) {
+    public Person save(@Validated @RequestBody Person person) {
         System.out.println(person);
         return person;
     }
-
 }
