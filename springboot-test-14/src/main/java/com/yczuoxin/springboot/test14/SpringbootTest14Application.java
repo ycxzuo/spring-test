31b6d723f5ad579104fc9065c784d045c9bbb103
zuoxin <343720929@qq.com>
2019-07-25 21:40:17 +0800

    使用xjc生成xsd对应的bean

---
+++ springboot-test-14/src/main/java/com/yczuoxin/springboot/test14/SpringbootTest14Application.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.springboot.test14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest14Application.class, args);
+    }
+
+}
