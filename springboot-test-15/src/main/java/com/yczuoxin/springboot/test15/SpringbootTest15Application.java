c41fbc91b52f45d5a593a931c3ee0f7745dcf97e
zuoxin <343720929@qq.com>
2019-08-01 23:02:25 +0800

    搭好服务

---
+++ springboot-test-15/src/main/java/com/yczuoxin/springboot/test15/SpringbootTest15Application.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.springboot.test15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest15Application.class, args);
+    }
+
+}
