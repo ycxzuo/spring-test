7eb73393dbd6c3e7d69fa173326987e2d25b0116
zuoxin <343720929@qq.com>
2019-08-31 18:13:57 +0800

    日志工程创建

---
+++ springboot-test-16/src/main/java/com/yczuoxin/springboot/test16/SpringbootTest16Application.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.springboot.test16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest16Application.class, args);
+    }
+
+}
